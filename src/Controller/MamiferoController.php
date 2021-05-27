7f5a1c9ec4072cc86443cee97b2ae4dbd1c03c01
lion-hearth <richard.oliveira1337@gmail.com>
2021-05-27 15:07:40 -0300

    project v2 atualização aula 9

--- src/Controller/MamiferoController.php
+++ src/Controller/MamiferoController.php
@@ -4,9 +4,10 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class MamiferoController
+class MamiferoController extends AbstractController
 {
     /**
      * @Route("/")
@@ -21,10 +22,17 @@ class MamiferoController
      */
      public function show($slug)
      {
-         return new Response(
-             sprintf(
-             'Esta é a página de detalhe do Mamífero "%s"  ' ,
-                ucwords( str_ireplace('-' , ' ', $slug) )
-             ));
+          $respostas = [
+              'Esta é a primeira resposta',
+              'Esta é a segunda resposta',
+              'Esta é a terceira resposta',
+              'Esta é a quarta resposta',
+              'Esta é a quinta resposta',
+          ];
+          return $this->render('mamiferos/show.html.twig', [
+              'animal' => ucwords( str_ireplace('-' , ' ', $slug)),
+               'respostas' => $respostas
+          ]);
+
      }
 }
