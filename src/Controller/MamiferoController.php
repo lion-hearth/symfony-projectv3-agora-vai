3eb09d1ec7b817f10184af443460c18c3480ba3a
lion-hearth <richard.oliveira1337@gmail.com>
2021-05-28 15:00:00 -0300

    project v2 atualização aula 10

--- src/Controller/MamiferoController.php
+++ src/Controller/MamiferoController.php
@@ -1,38 +1,41 @@
 <?php
 
-
 namespace App\Controller;
-
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 class MamiferoController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/mamifero", name="mamifero")
      */
-     public function homepage()
-     {
-         return new Response('Zoológico');
-     }
+    public function index(): Response
+    {
+        return $this->render('mamifero/index.html.twig', [
+            'controller_name' => 'MamiferoController',
+        ]);
+    }
 
     /**
      * @Route("/mamifero/{slug}")
      */
-     public function show($slug)
-     {
-          $respostas = [
-              'Esta é a primeira resposta',
-              'Esta é a segunda resposta',
-              'Esta é a terceira resposta',
-              'Esta é a quarta resposta',
-              'Esta é a quinta resposta',
-          ];
-          return $this->render('mamiferos/show.html.twig', [
-              'animal' => ucwords( str_ireplace('-' , ' ', $slug)),
-               'respostas' => $respostas
-          ]);
+    public function show($slug)
+    {
+        $respostas = [
+            'Esta é a primeira resposta',
+            'Esta é a segunda resposta',
+            'Esta é a terceira resposta',
+            'Esta é a quarta resposta',
+            'Esta é a quinta resposta',
+        ];
+        dump($respostas, $this);
+        return $this->render('mamiferos/show.html.twig', [
+            'animal' => ucwords( str_ireplace('-' , ' ', $slug)),
+            'respostas' => $respostas
+        ]);
 
-     }
+    }
+
 }
